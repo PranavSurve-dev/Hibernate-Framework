2c454ef06cfb2bb12f31178250433276b73b8ef6
PranavSurve-dev <pranavsurve444@gmail.com>
2026-08-26 21:15:28 +0530

    QueryParameter

---
+++ 02-EmpleeManagementSystem/src/main/java/Queryparameter/main.java
@@ -0,0 +1,36 @@
+package Queryparameter;
+
+import entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
+public class main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("surve");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		
+		String jpql = ("select e from Employee e where e.id = ?1 and e.name = ?2");
+		
+		Query query  = em.createQuery(jpql);
+		query.setParameter(1, 101);
+		query.setParameter(2, "Pranav");
+		
+		Employee emp  =(Employee)query.getSingleResult();
+		System.out.println(emp);
+		
+//String jpql = ("select e from Employee e where e.id = :empId and e.name = :empName");
+//		
+//		Query query  = em.createQuery(jpql);
+//		query.setParameter("empId", 101);
+//		query.setParameter("empName", "Pranav");
+//		
+//		Employee emp  =(Employee)query.getSingleResult();
+//		System.out.println(emp);
+		
+	}
+}
