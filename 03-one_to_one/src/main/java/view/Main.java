eb8a553cbcb1d4c4150d5f4976ab4a91caef63ab
PranavSurve-dev <pranavsurve444@gmail.com>
2026-08-28 21:08:14 +0530

    one-to one

---
+++ 03-one_to_one/src/main/java/view/Main.java
@@ -0,0 +1,30 @@
+package view;
+
+import entity.Car;
+import entity.Engine;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	  EntityManagerFactory emf  = Persistence.createEntityManagerFactory("pranav");
+	  EntityManager em = emf.createEntityManager();
+	  EntityTransaction et = em.getTransaction();
+	  
+	  
+	  Engine e1 = new Engine("petrol",1200); 
+	  Car c1 = new Car("BMW",10000,e1);
+	  
+	  et.begin();
+	  em.persist(e1);
+	  em.persist(c1);
+	  et.commit();
+	  
+	  
+		
+	}
+}
